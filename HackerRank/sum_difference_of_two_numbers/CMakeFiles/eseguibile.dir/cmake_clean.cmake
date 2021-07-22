57d7eda32d56177f0b39c621b4c03a4a0c907a25
Emanuele Gurini <emanuele.gurini89@gmail.com>
2021-07-22 18:12:57 +0200

    add cmake in sum_difference_of_two_numbers

---
+++ HackerRank/sum_difference_of_two_numbers/CMakeFiles/eseguibile.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eseguibile.dir/sum_difference_of_two_numbers.c.o"
+  "CMakeFiles/eseguibile.dir/sum_difference_of_two_numbers.c.o.d"
+  "eseguibile"
+  "eseguibile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eseguibile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
